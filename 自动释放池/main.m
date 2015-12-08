f7ca459226c4affcc54fc20fda18830c6206a81e
Miles <596442958@qq.com>
2015-12-08 16:31:28 +0800

    Initial Commit

---
+++ 自动释放池/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  自动释放池
+//
+//  Created by Miles on 12/8/15.
+//  Copyright © 2015 Miles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
